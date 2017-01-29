d5f2bf841e4deee000b482ac2354fc3b0d462103
esouthren <eilidhsouthren@gmail.com>
2017-01-29 23:45:26 +0000

    Update jsControllerTest.js

--- jsControllerTest.js
+++ jsControllerTest.js
@@ -45,7 +45,7 @@ angular.module('myApp', ['ngAnimate']).controller('MinesweeperController', funct
             if (length === 3) {
                 $scope.minefieldClass = "minefieldSmallTd";
                 $scope.difficulty = "Easy";
-                $scope.winMessage = "> Hey Alex! Nice one. See you soon. <3";
+                $scope.winMessage = "> Getting there! Why not try Medium?";
             }
             else if (length === 5) {
                 $scope.minefieldClass = "minefieldMediumTd";
